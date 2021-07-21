d2b4a8c9c2de2d5899ba6d5a093b624cea52658f
Sergey Kazantsev <sk.dlphn@gmail.com>
2021-07-21 19:03:24 +0600

    buttons redering

--- src/components/toolbar/Toolbar.js
+++ src/components/toolbar/Toolbar.js
@@ -1,6 +1,8 @@
-import { ExcelComponent } from '@core/ExcelComponent';
+import { createToolbar } from '@/components/toolbar/toolbar.template';
+import { $ } from '@core/dom';
+import { ExcelStateComponent } from '@core/ExcelStateComponent';
 
-export class Toolbar extends ExcelComponent {
+export class Toolbar extends ExcelStateComponent {
   static className = 'excel__toolbar';
 
   constructor($root, options) {
@@ -11,35 +13,33 @@ export class Toolbar extends ExcelComponent {
     });
   }
 
+  prepare() {
+    const initialState = {
+      textAlign: 'left',
+      fontWeight: 'normal',
+      fontStyle: 'normal',
+      textDecoration: 'none',
+    };
+    this.initState(initialState);
+  }
+
+  get template() {
+    return createToolbar(this.state);
+  }
+
   toHTML() {
-    return `
-      <div class="button">
-        <i class="material-icons">format_align_left</i>
-      </div>
-
-      <div class="button">
-        <i class="material-icons">format_align_center</i>
-      </div>
-
-      <div class="button">
-        <i class="material-icons">format_align_right</i>
-      </div>
-
-      <div class="button">
-        <i class="material-icons">format_bold</i>
-      </div>
-
-      <div class="button">
-        <i class="material-icons">format_italic</i>
-      </div>
-
-      <div class="button">
-        <i class="material-icons">format_underlined</i>
-      </div>
-    `;
+    return this.template;
   }
 
   onClick(event) {
-    console.log(event.target);
+    const $target = $(event.target);
+    if ($target.data.type === 'button') {
+      // const value = JSON.parse($target.data.value);
+      // const key = Object.keys(value)[0];
+      // this.setState({ [key]: value[key] });
+      this.setState(JSON.parse($target.data.value));
+
+      console.log(this.state);
+    }
   }
 }
